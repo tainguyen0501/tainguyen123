bb350e75d610aba064e3b69ede001fc2bdb23426
unknown <gduwtai@gmail.com>
2021-07-02 17:49:24 +0700

    first commit

---
+++ js/myJs.js
@@ -0,0 +1,133 @@
+$(document).ready(function() {
+    // process bar
+    setTimeout(function() {
+        firstQuestion();
+        $('.spinner').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow');
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    }, 600);
+})
+
+function firstQuestion(){
+    
+    $('.content').hide();
+    Swal.fire({
+        title: 'He luu bạn!',
+        text: 'Mình có điều này muốn hỏi bạn nhớ phải trả lời thật lòng nhaaa.',
+        imageUrl: 'img/ChangmieCute.jpg',
+        imageWidth: 300,
+        imageHeight: 300,
+        background: '#fff url("img/iput-bg.jpg")',
+        imageAlt: 'Custom image',
+      }).then(function(){
+        $('.content').show(200);
+      })
+}
+
+ // switch button position
+ function switchButton() {
+    var audio = new Audio('sound/duck.mp3');
+    audio.play();
+    var leftNo = $('#no').css("left");
+    var topNO = $('#no').css("top");
+    var leftY = $('#yes').css("left");
+    var topY = $('#yes').css("top");
+    $('#no').css("left", leftY);
+    $('#no').css("top", topY);
+    $('#yes').css("left", leftNo);
+    $('#yes').css("top", topNO);
+}
+// move random button póition
+function moveButton() {
+    var audio = new Audio('sound/Swish1.mp3');
+    audio.play();
+    if (screen.width<=600) {
+        var x = Math.random() * 300;
+        var y = Math.random() * 500;
+    } else{
+        var x = Math.random() * 500;
+        var y = Math.random() * 500;
+    }
+    var left = x + 'px';
+    var top = y + 'px';
+    $('#no').css("left", left);
+    $('#no').css("top", top);
+}
+
+
+var n = 0;
+$('#no').mousemove(function() {
+    if (n < 1)
+        switchButton();
+    if (n > 1)
+        moveButton();
+    n++;
+});
+$('#no').click(() => {
+    if (screen.width>=900)
+        switchButton();
+})
+
+// generate text in input
+function textGenerate() {
+    var n = "";
+    var text = " Changmie dễ thương sô ciu ku-te sĩuuuuuuuu :<<<<<< ";
+    var a = Array.from(text);
+    var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
+    var count = textVal.length;
+    if (count > 0) {
+        for (let i = 1; i <= count; i++) {
+            n = n + a[i];
+            if (i == text.length + 1) {
+                $('#txtReason').val("");
+                n = "";
+                break;
+            }
+        }
+    }
+    $('#txtReason').val(n);
+    setTimeout("textGenerate()", 1);
+}
+
+// show popup
+$('#yes').click(function() {
+    var audio = new Audio('sound/tick.mp3');
+    audio.play();
+    Swal.fire({
+        title: 'Nói cho mình lý do bạn thích Changmie đi :vvvv',
+        html: true,
+        width: 900,
+        padding: '3em',
+        html: "<input type='text' class='form-control' id='txtReason' onmousemove=textGenerate()  placeholder='Whyyy'>",
+        background: '#fff url("img/iput-bg.jpg")',
+        backdrop: `
+              rgba(0,0,123,0.4)
+              url("img/giphy2.gif")
+              left top
+              no-repeat
+            `,
+        showCancelButton: true,
+        cancelButtonText: " Nhấn nút gửi kìa nhấn này chi vậy :<<",
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonColor: '#fe8a71',
+        cancelButtonColor: '#f6cd61',
+        confirmButtonText: 'Gửi <3'
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire({
+                width: 900,
+                confirmButtonText: 'Okiiiii lun <3',
+                background: '#fff url("img/iput-bg.jpg")',
+                title: 'Cảm ơn mọi người đã yêu thương Changmie ^^  Yêu mọi người 3000 lần',
+                text: "Tối nay 19h30 Mie livestream trên tiktok @chang.mie nha cả nhà. À cảm ơn mọi người đã nghĩ mình nhạt nha <3",
+                confirmButtonColor: '#83d0c9',
+                onClose: () => {
+                    window.location = 'http://fb.com';
+                  }
+            })
+        }
+    })
+})
